ef2cd3335abe8a9702bb605607eb0ac92328b621
thang197 <thangnguyennvt1410@gmail.com>
2025-08-10 10:08:52 +0700

    F

---
+++ database/migrations/2025_08_10_100642_create_banner_groups_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBannerGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banner_groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timestamps();
+        });
+
+        Schema::create('banner_group_galleries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('group_id');
+            $table->integer('sort');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banner_groups');
+    }
+}
